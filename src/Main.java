1edffd0d982c9a34caffa24c090a08d97868cf7d
Bruno César <bcgmeireles@gmail.com>
2023-09-01 11:21:43 -0300

    feat: use primitive types

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,17 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Filme: Top Gun: Maverick");
 
-        int year;
+        int year = 2022;
+        System.out.println("Ano de lancamento: " + year);
+
+        boolean isInclude = true;
+
+        double rating = 8.1;
+
+        double average = ( 8.5 + 7.6 + 8.2 ) / 3;
+        System.out.println(average);
+
+        String resume;
+        resume = "Filme de aventura que apresenta alguns temas interessantes.";
     }
 }
